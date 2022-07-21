ad2284b9749de406191862a42a1475c1cc90b86c
Angelo Zammataro <juba.kem@gmail.com>
2022-07-21 18:26:37 +0200

    First Commit: Primi passi - First API - 01

---
+++ src/main/java/it2/develhope/firstAPI01/controllers/nameController.java
@@ -0,0 +1,21 @@
+package it2.develhope.firstAPI01.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class nameController {
+
+    @GetMapping
+    public String getName(){
+        return "Angelo Zammataro";
+    }
+
+    @PostMapping("/{name}")
+    public StringBuilder reverseName(@PathVariable String name){
+        StringBuilder str = new StringBuilder(name);
+        return str.reverse();
+    }
+}
---
+++ src/test/java/it2/develhope/firstAPI01/FirstApi01ApplicationTests.java
@@ -0,0 +1,13 @@
+package it2.develhope.firstAPI01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApi01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
